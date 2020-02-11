861b62d94ba2a0ea72425ae7bf5f6d653400c52b
Ben Dunjay <bendunjay@outlook.com>
2020-02-11 15:18:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,5 @@
 def find_element_index(array, value_to_find)
-counter = 0 
-while counter < array.length do
-  puts array[counter] if array[counter] === value_to_find
-   counter +=1
-end
+
  
  
  
